5697f7912c2142a44582e955aac5f4542ccb1f6a
jcbdoliveira <julio@adminformatica.com.br>
2023-01-10 20:56:26 -0300

    Resolução do desafio

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,25 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo \"{nomeApp}\" no seu {this.GetType().Name}");
+            //Console.WriteLine("");
+            for (int i = 0; i < 100; i++ )
+            {
+                Console.Write(".");
+                System.Threading.Thread.Sleep(100);
+            }
+
+            Console.WriteLine($"\n Aplicativo \"{nomeApp}\" instalado com sucesso!");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia1 = new Nokia("99888-7898", "Nokia C01 Plus", "100001-01-100001-1", 32);
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Trocar de número");
+nokia1.Numero = "999754725";
+Console.WriteLine($"Novo número: {nokia1.Numero}");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Iphone iphone1 = new Iphone ("99877-7976", "iPhone 14 Plus", "110011-11-110011-1", 128);
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Telegram");
